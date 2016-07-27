4c465e7d8b7658af3db36ce5098648c128091208
cello81 <junk@marcini.ch>
2016-07-27 10:49:28 +0200

    Allow development pc to access dev path. Add Lucky number tutorial files

---
+++ www/pv/src/AppBundle/Controller/LuckyController.php
@@ -0,0 +1,33 @@
+<?php
+// src/AppBundle/Controller/LuckyController.php
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class LuckyController extends Controller
+{
+     /**
+     * @Route("/lucky/number/{count}")
+     */
+    public function numberAction($count)
+    {
+        $numbers = array();
+        for ($i = 0; $i < $count; $i++) {
+            $numbers[] = rand(0, 100);
+        }
+        $numbersList = implode(', ', $numbers);
+/*
+        return new Response(
+            '<html><body>Lucky numbers: '.$numbersList.'</body></html>'
+        );
+*/
+	// render(): a shortcut that does the same as above
+	return $this->render(
+	 'lucky/number.html.twig',
+	 array('luckyNumberList' => $numbersList)
+          );
+    }
+
+}
