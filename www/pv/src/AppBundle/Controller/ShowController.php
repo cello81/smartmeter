bf4da34cc24ed0fa27868b4d7b0e82151ae7c1e6
cello81 <junk@marcini.ch>
2016-09-06 22:16:04 +0200

    Add multiple routes to display data in a time range

--- www/pv/src/AppBundle/Controller/ShowController.php
+++ www/pv/src/AppBundle/Controller/ShowController.php
@@ -6,43 +6,77 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use AppBundle\Entity\Rawdata;
 use DateTime;
+use DateInterval;
 
 class ShowController extends Controller
 {
+
      /**
-     * @Route("/show/meterdata/")
+     * @Route("/show/meterdata/{datetoshowfrom}/{datetoshowto}/")
      */
-    public function ShowMeterdataAction()
+
+    public function ShowMeterdataAction($datetoshowfrom, $datetoshowto)
     {
-        $em = $this->getDoctrine()->getManager();
-	$meterdataRepo = $em->getRepository('AppBundle:Rawdata');
+       $em = $this->getDoctrine()->getManager();
+       $meterdataRepo = $em->getRepository('AppBundle:Rawdata');
 
-	$prevTime = new DateTime('2016-08-03');
+       $prevTime = new DateTime($datetoshowfrom);
+//echo $prevTime->format('Y-m-d H:i:s');
+       if ($datetoshowto == 1)
+       {
+           $dateTimeEnd = new DateTime($datetoshowfrom);
+       }
+       else
+       {
+           $dateTimeEnd = new DateTime($datetoshowto);
+       }
+//echo $dateTimeEnd->format('Y-m-d H:i:s');
 
-	$query = $meterdataRepo->createQueryBuilder('p')
-   		->orderBy('p.id', 'ASC')
-		->where('p.measuringtime > :date')
-		->setParameter('date', $prevTime)
-		->getQuery();
+       $dateTimeEnd->add(new DateInterval('P1D'));  // + 1 Tag 
+//echo $dateTimeEnd->format('Y-m-d H:i:s');
 
-	$meterdataAll = $query->getResult();
+       $query = $meterdataRepo->createQueryBuilder('p')
+                ->orderBy('p.id', 'ASC')
+                ->where('p.measuringtime > :datefrom')
+                ->andWhere('p.measuringtime < :dateto')
+                ->setParameter('datefrom', $prevTime)
+                ->setParameter('dateto', $dateTimeEnd)
+                ->getQuery();
 
-        $prevTime = new DateTime('NOW');
+        $meterdataAll = $query->getResult();
 
         foreach ($meterdataAll as $mde) {
             $time = $mde->getMeasuringtime();
             $netflow = $mde->getNetflow();
             $mde->setTimediff($time->getTimestamp() - $prevTime->getTimestamp());
 
-	    if ($mde->getTimediff() != 0)
+            if ($mde->getTimediff() != 0)
                 $mde->setwattNet($netflow/($mde->GetTimediff()/3600));
             else
                 $mde->setwattNet(-1);
-	    $prevTime = $time;
-	}
+            $prevTime = $time;
+        }
 
-	return $this->render(
-		'show/meterentries.html.twig',
-		array('allmeterdata' => $meterdataAll));
+        return $this->render(
+                'show/meterentries.html.twig',
+                array('allmeterdata' => $meterdataAll));
+
+    }
+
+     /**
+     * @Route("/show/meterdata/")
+     */
+
+    public function ShowMeterdataActionToday()
+    {
+	return ShowController::ShowMeterdataAction('today',1);
+    }
+
+     /**
+     * @Route("/show/meterdata/all/")
+     */
+    public function ShowMeterdataActionAll()
+    {
+        return ShowController::ShowMeterdataAction('2016-08-04','NOW');
     }
 }
